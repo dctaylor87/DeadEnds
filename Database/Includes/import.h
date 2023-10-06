5266c4493c563353a3b108a260324339a793927d
Thomas Wetmore <ttw4@icloud.com>
2023-10-05 20:55:40 -0400

    added test of gedcom reading

---
+++ Database/Includes/import.h
@@ -0,0 +1,20 @@
+//
+//  DeadEnds
+//
+//  import.h -- Header file for the Gedcom import process.
+//
+//  Created by Thomas Wetmore on 13 November 2022.
+//  Last changed on 5 August 2023.
+//
+
+#ifndef import_h
+#define import_h
+
+#include <stdio.h>
+#include "recordindex.h"
+#include "errors.h"
+#include "database.h"
+
+bool importFromFiles(String fileNames[], int count, ErrorLog*);
+
+#endif // import_h
