22593703cca3d3641c20dc70ae43695551274759
David Taylor <dctaylor87@gmail.com>
2024-02-18 15:46:50 -0500

    White space and comments; nothing substantive.

--- LLSupport/Includes/browse.h
+++ LLSupport/Includes/browse.h
@@ -1,12 +1,14 @@
+/* browse.c */
 extern void init_browse_lists(void);
 extern void term_browse_lists(void);
 
+/* brwslist.c */
 extern void add_browse_list(String, Sequence *);
 extern void remove_browse_list(String, Sequence *);
 extern void new_name_browse_list(String, String);
 extern void update_browse_list(String, Sequence *);
 extern void remove_from_browse_lists(String);
 extern void rename_from_browse_lists(String);
-
 extern Sequence *find_named_seq(String);
+
 //extern void rename_indiseq(Sequence *, String);
