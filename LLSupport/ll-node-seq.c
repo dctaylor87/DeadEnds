4ce767e20a9c774f84aaf3e55e884d109f698488
David Taylor <dctaylor87@gmail.com>
2024-06-25 13:21:33 -0400

    Reindent; add whitespace to make more readable; make comments less obnoxious.

--- LLSupport/ll-node-seq.c
+++ LLSupport/ll-node-seq.c
@@ -22,11 +22,9 @@
 static void append_all_tags(Sequence *seq, GNode *node, CString tagname,
 			    bool recurse, bool nonptrs);
 
-/*=======================================================
- * append_all_tags -- append all tags of specified type
- *  to indiseq (optionally recursive
- * Created: 2000/11/29, Perry Rapp
- *=====================================================*/
+/* append_all_tags -- append all tags of a specified tagname
+   to Sequence (optionally recursive).  */
+
 static void
 append_all_tags(Sequence *seq, GNode *node, CString tagname,
 		bool recurse, bool nonptrs)
@@ -34,11 +32,13 @@ append_all_tags(Sequence *seq, GNode *node, CString tagname,
   if (!tagname || (ntag(node) && eqstr(ntag(node), tagname))) {
     String key;
     int val=0;
+
     key = nval(node);
     if (key && key[0]) {
       int keylen = strlen(key);
       String skey = 0;
       bool include=true;
+
       strupdate(&skey, rmvat(key));
       if (skey) {
 	val = atoi(skey+1);
@@ -46,6 +46,7 @@ append_all_tags(Sequence *seq, GNode *node, CString tagname,
 	if (nonptrs) {
 	  ZSTR zstr = zs_newn(keylen+100);
 	  GNode *chil;
+
 	  /* include non-pointers, but mark invalid with val==-1 */
 	  val = -1;
 	  zs_sets(zstr, key);
@@ -53,6 +54,7 @@ append_all_tags(Sequence *seq, GNode *node, CString tagname,
 	  for (chil = nchild(node); chil; chil=nsibling(chil)) {
 	    String text = nval(chil) ? nval(chil) : "";
 	    bool cr=false;
+
 	    if (eqstr_ex(ntag(chil), "CONC")) {
 	    } else if (eqstr_ex(ntag(chil), "CONT")) {
 	      cr=true;
@@ -78,65 +80,66 @@ append_all_tags(Sequence *seq, GNode *node, CString tagname,
     append_all_tags(seq, nchild(node), tagname, recurse, nonptrs);
   if (nsibling(node))
     append_all_tags(seq, nsibling(node), tagname, recurse, nonptrs);
-
 }
 
-/*=======================================================
- * node_to_sources -- Create sequence of all sources
- *  inside a node record (at any level)
- * Created: 2000/11/29, Perry Rapp
- *=====================================================*/
+/* GNodeToSources -- Create a sequence of all sources
+   mentioned inside a node tree (at any level).  */
+
 Sequence *
 GNodeToSources (GNode *node, Database *database)
 {
-	Sequence *seq;
-	if (!node) return NULL;
-	seq = createSequence(database);
-	append_all_tags(seq, node, "SOUR", true, true);
-	if (!lengthSequence(seq))
-	{
-		deleteSequence(seq);
-		seq = NULL;
-	}
-	return seq;
+  Sequence *seq;
+
+  if (!node)
+    return NULL;
+  seq = createSequence(database);
+  append_all_tags(seq, node, "SOUR", true, true);
+  if (!lengthSequence(seq))
+    {
+      deleteSequence(seq);
+      seq = NULL;
+    }
+  return seq;
 }
 
-/*=======================================================
- * node_to_notes -- Create sequence of all notes
- *  inside a node record (at any level)
- * 2001/02/11, Perry Rapp
- *=====================================================*/
+/* GNodeToNotes -- Create a sequence of all notes
+   inside a node tree (at any level).  */
+
 Sequence *
 GNodeToNotes (GNode *node, Database *database)
 {
-	Sequence *seq;
-	if (!node) return NULL;
-	seq = createSequence(database);
-	append_all_tags(seq, node, "NOTE", true, true);
-	if (! lengthSequence(seq))
-	{
-		deleteSequence(seq);
-		seq = NULL;
-	}
-	return seq;
+  Sequence *seq;
+
+  if (!node)
+    return NULL;
+
+  seq = createSequence(database);
+  append_all_tags(seq, node, "NOTE", true, true);
+  if (! lengthSequence(seq))
+    {
+      deleteSequence(seq);
+      seq = NULL;
+    }
+  return seq;
 }
 
-/*=======================================================
- * node_to_pointers -- Create sequence of all pointers
- *  inside a node record (at any level)
- * 2001/02/24, Perry Rapp
- *=====================================================*/
+/* GNodeToPointers -- Create a sequence of all pointers
+   inside a node tree (at any level).  */
+
 Sequence *
 GNodeToPointers (GNode *node, Database *database)
 {
-	Sequence *seq;
-	if (!node) return NULL;
-	seq = createSequence(database);
-	append_all_tags(seq, node, NULL, true, false);
-	if (! lengthSequence(seq))
-	{
-		deleteSequence(seq);
-		seq = NULL;
-	}
-	return seq;
+  Sequence *seq;
+
+  if (!node)
+    return NULL;
+
+  seq = createSequence(database);
+  append_all_tags(seq, node, NULL, true, false);
+  if (! lengthSequence(seq))
+    {
+      deleteSequence(seq);
+      seq = NULL;
+    }
+  return seq;
 }
