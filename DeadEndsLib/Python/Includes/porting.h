3f7927f26b918ceaa0ec2908356674df7c68294c
David Taylor <dctaylor87@gmail.com>
2024-08-29 15:33:19 -0400

    delete some dead code.

--- DeadEndsLib/Python/Includes/porting.h
+++ DeadEndsLib/Python/Includes/porting.h
@@ -9,27 +9,6 @@
 #define SURCAPTYPE	_Bool
 //#define NODE		GNode *
 
-/* nxref, nflag, ncel are not used by Python code.  The fate of
-   nrefcnt is yet to be determined -- it is used by Python code, but
-   is not defined by DeadEnds.  */
-
-//#define nchild(node)	((node)->child)
-//#define nsibling(node)	((node)->sibling)
-//#define nparent(node)	((node)->parent)
-//#define nval(node)	((node)->value)
-//#define ntag(node)	((node)->tag)
-//#define nxref(node)	((node)->key)
-
-//#if 0
-//#define nrefcnt_inc(node)	/* XXX currently no field, but we need this XXX*/
-//#define nrefcnt_dec(node)	/* XXX currently no field, but we need this XXX*/
-//#define get_nrefcnt(node)	/* XXX currently no field, but we need this XXX*/
-//#else
-//#define nrefcnt_inc(node)	((node)->refcount++)
-//#define nrefcnt_dec(node)	((node)->refcount--)
-//#define get_nrefcnt(node)	((node)->refcount)
-//#endif
-
 #define TRACK_NODE_REFCNT_INC(node) /* empty -- no current ref counts */
 #define TRACK_NODE_REFCNT_DEC(node) /* empty -- no current ref counts */
 
