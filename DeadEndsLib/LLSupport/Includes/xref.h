2e279f28d3b8ae2458021dd809cb5def41d15523
David Taylor <dctaylor87@gmail.com>
2024-09-19 11:24:45 -0400

    new function decls; some do not exist yet.

--- DeadEndsLib/LLSupport/Includes/xref.h
+++ DeadEndsLib/LLSupport/Includes/xref.h
@@ -10,10 +10,21 @@ extern RecordIndexEl *getNextSourceRecord (CString key, Database *database);
 extern RecordIndexEl *getNextEventRecord (CString key, Database *database);
 extern RecordIndexEl *getNextOtherRecord (CString key, Database *database);
 
+extern RecordIndexEl *getPreviousPersonRecord (CString key, Database *database);
+extern RecordIndexEl *getPreviousFamilyRecord (CString key, Database *database);
+extern RecordIndexEl *getPreviousSourceRecord (CString key, Database *database);
+extern RecordIndexEl *getPreviousEventRecord (CString key, Database *database);
+extern RecordIndexEl *getPreviousOtherRecord (CString key, Database *database);
+
 extern CString getNewPersonKey (Database *database);
 extern CString getNewFamilyKey (Database *database);
 extern CString getNewSourceKey (Database *database);
 extern CString getNewEventKey (Database *database);
 extern CString getNewOtherKey (Database *database);
+
+extern RecordIndexEl *getFirstRecord (RecordType type, Database *database);
+extern RecordIndexEl *getLastRecord (RecordType type, Database *database);
+extern RecordIndexEl *getNextRecord (RecordType type, CString key, Database *database);
+extern RecordIndexEl *getPreviousRecord (RecordType type, CString key, Database *database);
 
 extern bool isKeyInUse (CString key, Database *database);
