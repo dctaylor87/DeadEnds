7cae3db52c7edafee79538b7f470e8301b3a4717
David Taylor <dctaylor87@gmail.com>
2024-09-25 17:40:00 -0400

    Remove decl's of functions in equaliso.c (no longer part of the build).

--- DeadEndsLib/LLSupport/Includes/editing.h
+++ DeadEndsLib/LLSupport/Includes/editing.h
@@ -5,10 +5,3 @@ extern void write_fam_to_file_for_edit(GNode *fam, CString file, bool rfmt, Data
 extern void write_indi_to_file_for_edit(GNode *indi, CString file, bool rfmt, Database *database);
 extern void write_nodes(int, FILE*, XLAT, GNode *, bool, bool, bool);
 extern void write_node_to_editfile(GNode *);
-
-extern bool equal_tree(GNode *, GNode *);
-extern bool equal_node(GNode *, GNode *);
-extern bool equal_nodes(GNode *, GNode *, bool, bool);
-
-extern bool iso_list(GNode *, GNode *);
-extern bool iso_nodes(GNode *, GNode *, bool, bool );
