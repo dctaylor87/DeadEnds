bbaac13860cad79fbf200e7ccfe0b06e13374083
David Taylor <dctaylor87@gmail.com>
2024-11-03 13:53:36 -0500

    Delete some dead code.

--- Lifelines/screen.c
+++ Lifelines/screen.c
@@ -1083,49 +1083,6 @@ list_browse (Sequence *seq, int top, int * cur, int mark)
 	return interact_choice_string(main_win, "jkeimrtbanx$^udUDq");
 }
 
-#if !defined(DEADENDS)
-/*======================================
- * ask_for_db_filename -- Ask user for DeadEnds database directory
- *  ttl:   [IN]  title of question (1rst line)
- *  prmpt: [IN]  prompt of question (2nd line)
- *====================================*/
-bool
-ask_for_db_filename (CString ttl, CString prmpt, ATTRIBUTE_UNUSED CString basedir, String buffer, int buflen)
-{
-	/* This could have a list of existing ones like askprogram.c */
-	return ask_for_string(ttl, prmpt, buffer, buflen);
-}
-
-/*======================================
- * ask_for_output_filename -- Ask user for filename to which to write
- *  returns static buffer
- *  ttl1:    [IN]  title of question (1rst line)
- *  prmpt:   [IN]  prompt of question (3rd line)
- *  buffer:  [OUT] response
- *  buflen:  [IN]  max size of response
- *====================================*/
-bool
-ask_for_output_filename (CString ttl, CString path, CString prmpt, String buffer, int buflen)
-{
-	/* curses version doesn't differentiate input from output prompts */
-	return ask_for_filename_impl(ttl, path, prmpt, buffer, buflen);
-}
-/*======================================
- * ask_for_input_filename -- Ask user for filename from which to read
- *  returns static buffer
- *  ttl1:    [IN]  title of question (1rst line)
- *  prmpt:   [IN]  prompt of question (3rd line)
- *  buffer:  [OUT] response
- *  buflen:  [IN]  max size of response
- *====================================*/
-bool
-ask_for_input_filename (CString ttl, CString path, CString prmpt, String buffer, int buflen)
-{
-	/* curses version doesn't differentiate input from output prompts */
-	return ask_for_filename_impl(ttl, path, prmpt, buffer, buflen);
-}
-#endif
-
 /*======================================
  * ask_for_input_filename_impl -- Ask user for a filename
  *  (in curses version, we don't differentiate input from output prompts)
@@ -1210,56 +1167,6 @@ ask_for_string2 (CString ttl1, CString ttl2, CString prmpt, String buffer, int b
 	return rtn;
 }
 
-#if !defined(DEADENDS)
-/*========================================
- * yes_no_value -- Convert character to true if y(es)
- *======================================*/
-bool
-yes_no_value (int c)
-{
-	String ptr;
-	for (ptr = _(qSaskyY); *ptr; ptr++) {
-		if (c == *ptr) return true;
-	}
-	return false;
-}
-
-/*========================================
- * ask_yes_or_no -- Ask yes or no question
- *  ttl:  [IN]  title to display
- *======================================*/
-bool
-ask_yes_or_no (CString ttl)
-{
-	int c = ask_for_char(ttl, _(qSaskynq), _(qSaskynyn));
-	return yes_no_value(c);
-}
-
-/*=========================================================
- * ask_yes_or_no_msg -- Ask yes or no question with message
- *  msg:   [IN]  top line displayed
- *  ttl:   [IN]  2nd line displayed
- *=======================================================*/
-bool
-ask_yes_or_no_msg (CString msg, CString ttl)
-{
-	int c = ask_for_char_msg(msg, ttl, _(qSaskynq), _(qSaskynyn));
-	return yes_no_value(c);
-}
-
-/*=======================================
- * ask_for_char -- Ask user for character
- *  ttl:   [IN]  1nd line displayed
- *  prmpt: [IN]  2nd line text before cursor
- *  ptrn:  [IN]  List of allowable character responses
- *=====================================*/
-int
-ask_for_char (CString ttl, CString prmpt, CString ptrn)
-{
-	return ask_for_char_msg(NULL, ttl, prmpt, ptrn);
-}
-#endif
-
 /*===========================================
  * ask_for_char_msg -- Ask user for character
  *  msg:   [IN]  top line displayed (optional)
@@ -1312,42 +1219,6 @@ display_list (CString ttl, List *list)
 	return chooseFromList(ttl, list);
 }
 
-#if !defined(DEADENDS)
-/*============================================
- * chooseFromList -- Choose from string list
- *  ttl:    [IN] title for display
- *  list    [IN] list of string choices
- * returns 0-based index chosen, or -1 if cancelled
- *==========================================*/
-int
-chooseFromList (CString ttl, List *list)
-{
-	String * array=0;
-	String choice=0;
-	int i=0, rtn=-1;
-	int len = lengthList(list);
-
-	if (len < 1) return -1;
-	if (!ttl) ttl=_(qSdefttl);
-
-	array = (String *) stdalloc(len*sizeof(String));
-	i = 0;
-	FORLIST(list, el)
-		choice = (String)el;
-		ASSERT(choice);
-		array[i] = strsave(choice);
-		++i;
-	ENDLIST
-
-	rtn = chooseFromArray(ttl, len, array);
-
-	for (i=0; i<len; ++i)
-		strfree(&array[i]);
-	stdfree(array);
-	return rtn;
-}
-#endif
-
 /*============================================
  * chooseFromArray_x -- Choose from string list
  *  ttl:      [IN]  title for choice display
@@ -1397,34 +1268,6 @@ chooseOrViewArray (CString ttl, int no, String *pstrngs, bool selecting
 	refresh_main();
 	return rv;
 }
-
-#if !defined(DEADENDS)
-/*=============================================================
- * chooseOneFromSequence --
- * Choose a single person from indiseq
- * Returns index of selected item (or -1 if user quit)
- *  ttl:  [IN]  title
- *===========================================================*/
-int
-chooseOneFromSequence (CString ttl, Sequence *seq)
-{
-	return chooseOneOrListFromSequence(ttl, seq, false);
-}
-/*==========================================================
- * chooseListFromSequence -- User chooses subsequence from
- *   person sequence
- * returns input sequence, but may have deleted elements
- * called by both reports & interactive use
- *  ttl:  [IN]  title/caption for choice list
- *  seq:  [I/O] list from which to choose (user may delete items)
- * returns index of where user choose select (or -1 if quit)
- *========================================================*/
-int
-chooseListFromSequence (CString ttl, Sequence *seq)
-{
-	return chooseOneOrListFromSequence(ttl, seq, true);
-}
-#endif
 
 /*==============================
  * draw_tt_win -- Draw menu for edit translations
