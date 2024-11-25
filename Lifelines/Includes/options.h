68286b2a4b78e31c2244c8cea2f37cf57ec6bd5a
David Taylor <dctaylor87@gmail.com>
2024-11-25 09:56:58 -0500

    New decl's -- preparation for splitting out parts of llexec.c and main.c.

--- Lifelines/Includes/options.h
+++ Lifelines/Includes/options.h
@@ -9,4 +9,22 @@ extern bool keyflag;		/* whether to display key values */
 extern int winx;		/* screen size, columns */
 extern int winy;		/* screen size, rows */
 
+extern bool readonly;		/* database is read-only -- no modifications allowed */
+
+extern bool debugmode;		/* no signal handling, so we can get coredump */
+
+extern bool keyflag;		/* show key values */
+extern bool traditional;	/* use traditional family rules */
+
+extern List *exprogs;
+extern StringTable *exargs;
+extern String progout;
+extern bool graphical;
+extern String configfile;
+
+extern bool python_interactive;
+extern bool have_python_scripts;
+
+extern CString usageSummary;
+
 #endif
