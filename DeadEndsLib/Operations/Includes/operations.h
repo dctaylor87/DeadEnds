cdaa8ca3c591f8f3ddd040fe6fe92616edb4916b
David Taylor <dctaylor87@gmail.com>
2025-01-06 18:13:51 -0500

    Remove two files that should have been removed by a previous merge.

--- DeadEndsLib/Operations/Includes/operations.h
+++
@@ -1,3 +0,0 @@
-extern bool addChildToFamily (GNode *child, GNode *family, int index, Database *database);
-extern bool addSpouseToFamily (GNode* spouse, GNode* family, SexType sext, Database* database);
-extern GNode* createFamily(GNode* husb, GNode* wife, GNode* chil, GNode* rest, Database* database);
