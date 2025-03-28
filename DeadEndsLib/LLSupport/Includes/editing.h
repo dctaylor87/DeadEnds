a061145d1dab16fdb4fc527c97f596fe68eb7aa3
David Taylor <dctaylor87@gmail.com>
2025-03-28 16:04:30 -0400

    Declare prefix_file_for_gedcom, prefix_file_for_report, write_indi_to_file.

--- DeadEndsLib/LLSupport/Includes/editing.h
+++ DeadEndsLib/LLSupport/Includes/editing.h
@@ -1,6 +1,9 @@
 #define NAMESEP		'/'	/* separates surname from the rest of the name */
 
 extern void prefix_file_for_edit (FILE *fp);
+extern void prefix_file_for_gedcom (FILE *fp);
+extern void prefix_file_for_report (FILE *fp);
+extern void write_indi_to_file (GNode *indi, CString file);
 extern void write_fam_to_file_for_edit(GNode *fam, CString file, bool rfmt, Database *database);
 extern void write_indi_to_file_for_edit(GNode *indi, CString file, bool rfmt, Database *database);
 extern void write_nodes(int, FILE*, XLAT, GNode *, bool, bool, bool);
