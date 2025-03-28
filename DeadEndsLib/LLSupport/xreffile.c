1d25f2fc4800228665b9dfb9ca916a958f7e3c24
David Taylor <dctaylor87@gmail.com>
2025-03-28 17:22:58 -0400

    New comment.  Remove some #if'ed out code.

--- DeadEndsLib/LLSupport/xreffile.c
+++ DeadEndsLib/LLSupport/xreffile.c
@@ -21,6 +21,25 @@
    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
 */
+
+/* xreffile.c -- handles xrefs.
+
+   In LifeLines, at startup this consulted the on-disk database to
+   determine which keys were in use and which were free.  It stored,
+   in memory, for each key type, a list of the free keys and the
+   highest number key in use.
+
+   In DeadEnds, by contrast, we do not currently keep track of free vs
+   in-use keys.  Instead, we assume that the database, when created,
+   has mostly sequential keys.  We record the highest key currently
+   allocated.  When we need to allocate a new key, we allocate at the
+   top, bumping the max key by one.  If this becomes a problem, we
+   might someday keep a list of dead keys.
+
+   Additionally, in LifeLines, there was only one database.  In
+   DeadEnds, there can be multiple databases, so the database needs to
+   be an argument.  */
+
 /*=============================================================
  * xreffile.c -- Handle the xref file
  * This module manages the lists of free record numbers
@@ -111,22 +130,11 @@ static bool addxref_impl(CString key, DUPS dups);
 static bool addxxref_impl(int32_t key, DUPS dups);
 static void dumpxrecs (CString type, DELETESET set, int32_t *offset);
 static int32_t find_slot(int32_t keynum, DELETESET set);
-#if !defined(DEADENDS)
-static void freexref(DELETESET set);
-#endif
 static DELETESET get_deleteset_from_type(char ctype);
 static String getxref(DELETESET set);
 static void growxrefs(DELETESET set);
 static String newxref(CString xrefp, bool flag, DELETESET set);
-#if !defined(DEADENDS)
-static int num_set(DELETESET set);
-#endif
 static bool parse_key(CString key, char * ktype, int32_t * kval);
-#if !defined(DEADENDS)
-static void readrecs(DELETESET set);
-static bool readxrefs(void);
-#endif
-static bool xref_isvalid_impl(DELETESET set, int32_t keynum);
 static int xref_last(DELETESET set);
 
 /*********************************************
@@ -136,10 +144,6 @@ static int xref_last(DELETESET set);
 /* INDI, FAM, EVEN, SOUR, other sets */
 static struct deleteset_s irecs, frecs, srecs, erecs, xrecs;
 
-#if !defined(DEADENDS)
-static FILE *xreffp=0;	/* open xref file pointer */
-static bool xrefReadonly = false;
-#endif
 static int xrefsize=0; /* xref file size */
 
 static int32_t maxkeynum=-1; /* cache value of largest key extant (-1 means not sure) */
@@ -148,94 +152,6 @@ static int32_t maxkeynum=-1; /* cache value of largest key extant (-1 means not 
  * local & exported function definitions
  * body of module
  *********************************************/
-
-#if !defined(DEADENDS)
-/*==================================== 
- * initdset -- Initialize a delete set
- *==================================*/
-static void
-initdset (DELETESET set, char ctype)
-{
-	set->ctype = ctype;
-	set->max = 0;
-	set->n = 1;
-	set->recs = 0;
-}
-/*=================================== 
- * initdsets -- Initialize delete sets
- *=================================*/
-static void
-initdsets (void)
-{
-	initdset(&irecs, 'I');
-	initdset(&frecs, 'F');
-	initdset(&srecs, 'S');
-	initdset(&erecs, 'E');
-	initdset(&xrecs, 'X');
-}
-
-/*============================== 
- * initxref -- Create xrefs file
- *============================*/
-void
-initxref (void)
-{
-	char scratch[MAXPATHLEN];
-	int32_t i = 1;
-	int j;
-	ASSERT(!xrefReadonly);
-	initdsets();
-	ASSERT(!xreffp);
-	snprintf(scratch, sizeof(scratch), "%s/xrefs", BTR->b_basedir);
-	ASSERT(xreffp = fopen(scratch, DEWRITEBINARY));
-	for (j = 0; j < 10; j++) {
-		ASSERT(fwrite(&i, sizeof(int32_t), 1, xreffp) == 1);
-	}
-	fclose(xreffp); xreffp=0;
-}
-
-/*============================
- * openxref -- Open xrefs file
- *==========================*/
-bool
-openxref (bool readonly)
-{
-	char scratch[MAXPATHLEN];
-	String fmode;
-	bool success;
-
-	initdsets();
-	ASSERT(!xreffp);
-	snprintf(scratch, sizeof(scratch), "%s/xrefs", BTR->b_basedir);
-	xrefReadonly = readonly;
-	fmode = xrefReadonly ? DEREADBINARY : DEREADBINARYUPDATE;
-	if (!(xreffp = fopen(scratch, fmode))) {
-		return false;
-	}
-
-	success = readxrefs();
-
-	xrefsize = ftell(xreffp);
-
-	return success;
-}
-/*==============================
- * closexref -- Close xrefs file
- *  (Safe to call if not open)
- *============================*/
-void
-closexref (void)
-{
-	if (xreffp) {
-		fclose(xreffp); xreffp = 0;
-	}
-	freexref(&irecs);
-	freexref(&frecs);
-	freexref(&srecs);
-	freexref(&erecs);
-	freexref(&xrecs);
-}
-#endif
 
 /*=========================================
  * getxrefnum -- Return new keynum for type
@@ -248,9 +164,6 @@ static int32_t
 getxrefnum (DELETESET set)
 {
 	int32_t keynum;
-#if !defined(DEADENDS)
-	ASSERT(xreffp);
-#endif
 	ASSERT(set->n >= 1);
 	if (set->n == 1) {
 		/* no free records on list */
@@ -264,9 +177,6 @@ getxrefnum (DELETESET set)
 		/* remove just-used entry from list */
 		--(set->n);
 	}
-#if !defined(DEADENDS)
-	ASSERT(writexrefs());
-#endif
 	maxkeynum=-1;
 	return keynum;
 }
@@ -300,117 +210,6 @@ String getxxref (void) { return getxref(&xrecs); }
  * Should try to revert back to getixref as above.
  *=================================================*/
 int32_t getixrefnum (void) { return getxrefnum(&irecs); }
-
-#if !defined(DEADENDS)
-/*======================================
- * sortxref -- Sort xrefs after reading
- *====================================*/
-static void
-sortxref (DELETESET set)
-{
-	/*
-	TO DO - call qsort instead
-	and also flag sorted file by changing file structure
-	*/
-	/*
-	sort from high to low, so lowest at top of
-	array, ready to be handed out
-
-	they should normally already be sorted, 
-	so use watchful bubble-sort for O(n)
-	*/
-	int32_t i, j, temp;
-	bool sorted;
-	for (i=1; i<set->n; i++) {
-		sorted = true;
-		for (j=i+1; j<set->n; j++) {
-			if (set->recs[i] < set->recs[j]) {
-				sorted = false;
-				temp = set->recs[j];
-				set->recs[j] = set->recs[i];
-				set->recs[i] = temp;
-			}
-		}
-		if (i==1 && sorted) return; /* already sorted */
-	}
-}
-/*======================================
- * sortxrefs -- Sort xrefs after reading
- *====================================*/
-static void
-sortxrefs (void)
-{
-	sortxref(&irecs);
-	sortxref(&frecs);
-	sortxref(&srecs);
-	sortxref(&erecs);
-	sortxref(&xrecs);
-}
-
-/*=============================
- * readxrefs -- Read xrefs file
- *  storage order: IFESX
- *===========================*/
-static bool
-readxrefs (void)
-{
-	ASSERT(xreffp);
-	ASSERT(fread(&irecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fread(&frecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fread(&erecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fread(&srecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fread(&xrecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(irecs.n > 0);
-	ASSERT(frecs.n > 0);
-	ASSERT(erecs.n > 0);
-	ASSERT(srecs.n > 0);
-	ASSERT(xrecs.n > 0);
-	if (irecs.n > irecs.max) growxrefs(&irecs);
-	if (frecs.n > frecs.max) growxrefs(&frecs);
-	if (srecs.n > srecs.max) growxrefs(&srecs);
-	if (erecs.n > erecs.max) growxrefs(&erecs);
-	if (xrecs.n > xrecs.max) growxrefs(&xrecs);
-	readrecs(&irecs);
-	readrecs(&frecs);
-	readrecs(&erecs);
-	readrecs(&srecs);
-	readrecs(&xrecs);
-	sortxrefs();
-	return true;
-}
-/*=========================================
- * readrecs -- Read in one array of records
- *=======================================*/
-static void
-readrecs (DELETESET set)
-{
-	ASSERT((int32_t)fread(set->recs, sizeof(int32_t), set->n, xreffp) == set->n);
-}
-
-/*================================
- * writexrefs -- Write xrefs file.
- *  storage order: IFESX
- *==============================*/
-bool
-writexrefs (void)
-{
-	ASSERT(!xrefReadonly);
-	ASSERT(xreffp);
-	rewind(xreffp);
-	ASSERT(fwrite(&irecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fwrite(&frecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fwrite(&erecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fwrite(&srecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT(fwrite(&xrecs.n, sizeof(int32_t), 1, xreffp) == 1);
-	ASSERT((int32_t)fwrite(irecs.recs, sizeof(int32_t), irecs.n, xreffp) == irecs.n);
-	ASSERT((int32_t)fwrite(frecs.recs, sizeof(int32_t), frecs.n, xreffp) == frecs.n);
-	ASSERT((int32_t)fwrite(erecs.recs, sizeof(int32_t), erecs.n, xreffp) == erecs.n);
-	ASSERT((int32_t)fwrite(srecs.recs, sizeof(int32_t), srecs.n, xreffp) == srecs.n);
-	ASSERT((int32_t)fwrite(xrecs.recs, sizeof(int32_t), xrecs.n, xreffp) == xrecs.n);
-	fflush(xreffp);
-	return true;
-}
-#endif
 
 /*================================
  * dumpxrefs -- Print xrefs to stdout
@@ -504,9 +303,6 @@ add_xref_to_set_impl (int32_t keynum, DELETESET set, DUPS dups)
 		add this to the list
 		*/
 		--set->recs[0];
-#if !defined(DEADENDS)
-		ASSERT(writexrefs());
-#endif
 		return true;
 	}
 	if (set->n >= set->max)
@@ -529,9 +325,6 @@ add_xref_to_set_impl (int32_t keynum, DELETESET set, DUPS dups)
 		(set->recs)[i+1] = (set->recs)[i];
 	(set->recs)[lo] = keynum;
 	(set->n)++;
-#if !defined(DEADENDS)
-	ASSERT(writexrefs());
-#endif
 	maxkeynum=-1;
 	return true;
 }
@@ -554,49 +347,6 @@ void addfxref (int key) { addfxref_impl((int32_t)key, NODUPS); }
 void addsxref (int key) { addsxref_impl((int32_t)key, NODUPS); }
 void addexref (int key) { addexref_impl((int32_t)key, NODUPS); }
 void addxxref (int key) { addxxref_impl((int32_t)key, NODUPS); }
-
-#if !defined(DEADENDS)
-/*===================================================
- * addxref_impl -- Mark key free (accepts string key, any type)
- *  key:    [IN]  key to delete (add to free set)
- *  silent: [IN]  if false, ASSERT if record is already free
- *=================================================*/
-static bool
-addxref_impl (CString key, DUPS dups)
-{
-	char ktype=0;
-	int32_t keynum=0;
-	if (!parse_key(key, &ktype, &keynum)) {
-		char msg[512];
-		snprintf(msg, sizeof(msg), "Bad key passed to addxref_impl: %s", key);
-		fatal(msg);
-	}
-	switch(ktype) {
-	case 'I': return addixref_impl(keynum, dups);
-	case 'F': return addfxref_impl(keynum, dups);
-	case 'S': return addsxref_impl(keynum, dups);
-	case 'E': return addexref_impl(keynum, dups);
-	case 'X': return addxxref_impl(keynum, dups);
-	default: ASSERT(0); return false;
-	}
-}
-/*===================================================
- * addxref -- Mark key free (accepts string key, any type)
- * ASSERT if key is already free
- *=================================================*/
-void addxref (CString key)
-{
-	addxref_impl(key, NODUPS);
-}
-/*===================================================
- * addxref_if_missing -- Mark key free (accepts string key, any type)
- * Does nothing if key already free
- *=================================================*/
-bool addxref_if_missing (CString key)
-{
-	return addxref_impl(key, DUPSOK);
-}
-#endif
 
 /*==========================================
  * growxrefs -- Grow memory for xrefs array.
@@ -663,9 +413,6 @@ delete_xref_if_present (CString key)
 	if (set->n > 1)
 		set->recs[set->n - 1] = 0;
 	--(set->n);
-#if !defined(DEADENDS)
-	ASSERT(writexrefs());
-#endif
 	maxkeynum=-1;
 	return true;
 
@@ -686,58 +433,6 @@ parse_key (CString key, char * ktype, int32_t * kval)
 	*kval = atoi(key+1);
 	return true;
 }
-
-#if !defined(DEADENDS)	  /* no callers */
-/*==========================================
- * is_key_in_use -- Return true if a live record
- *========================================*/
-bool
-is_key_in_use (CString key)
-{
-	DELETESET set=0;
-	int32_t keynum=0;
-	char ktype=0;
-	CString barekey=0;
-	bool result=false;
-
-	if (!parse_key(key, &ktype, &keynum)) {
-		char msg[512];
-		snprintf(msg, sizeof(msg), "Bad key passed to is_key_in_use: %s", key);
-		fatal(msg);
-	}
-
-	set = get_deleteset_from_type(ktype);
-
-	ASSERT(keynum>0);
-	
-	result = xref_isvalid_impl(set, keynum);
-
-	strfree((String *)&barekey);
-	
-	return result;
-}
-#endif
-
-#if !defined(DEADENDS)
-/*==========================================
- * freexref -- Free memory & clear xrefs array
- *  Called when database is closed
- *========================================*/
-static void
-freexref (DELETESET set)
-{
-	ASSERT(set);
-	if (set->recs) {
-		stdfree(set->recs);
-		set->recs = 0;
-		set->max = 0;
-		set->n = 0;
-	} else {
-		ASSERT(set->max == 0);
-		ASSERT(set->n == 0);
-	}
-}
-#endif
 
 /*==========================================================
  * xref_num_????s -- Return number of type of things in database.
@@ -797,23 +492,13 @@ static String
 newxref (CString xrefp, bool flag, DELETESET set)
 {
 	int32_t keynum;
-#if !defined(DEADENDS)
-	bool changed;
-#endif
 	static char scratch[12];
 	if(flag) {
 		keynum = atoi(xrefp+1);
-#if !defined(DEADENDS)
-		changed = ((set->n != 1) || (keynum >= set->recs[0]));
-#endif
 		if(set->n != 1)
 			set->n = 1;	/* forget about deleted entries */
 		if(keynum >= set->recs[0])
 			set->recs[0] = keynum+1;	/* next available */
-#if !defined(DEADENDS)
-		if(changed)
-			ASSERT(writexrefs());
-#endif
 		snprintf(scratch, sizeof(scratch), "@%s@", xrefp);
 		return(scratch);
 	}
@@ -849,183 +534,3 @@ newxxref (CString xrefp, bool flag)
 {
 	return newxref(xrefp, flag, &xrecs);
 }
-/*================================================
- * xref_isvalid_impl -- is this a valid whatever ?
- *  generic for all 5 types
- * (internal use)
- *==============================================*/
-static bool
-xref_isvalid_impl (DELETESET set, int32_t keynum)
-{
-	int32_t lo,hi,md;
-	if (set->n == set->recs[0]) return false; /* no valids */
-	if (set->n == 1) return true; /* all valid */
-	/* binary search deleteds */
-	lo=1;
-	hi=(set->n)-1;
-	while (lo<=hi) {
-		md = (lo + hi)/2;
-		if (keynum>(set->recs)[md])
-			hi=--md;
-		else if (keynum<(set->recs)[md])
-			lo=++md;
-		else
-			return false;
-	}
-	return true;
-}
-/*=========================================================
- * xref_next_impl -- Return next valid of some type after i
- *  returns 0 if none found
- *  generic for all 5 types
- *  this could be more efficient (after first one work
- *  thru tree)
- *=======================================================*/
-static int32_t
-xref_next_impl (DELETESET set, int32_t i)
-{
-	if (set->n == set->recs[0]) return 0; /* no valids */
-	while (++i < set->recs[0])
-	{
-		if (xref_isvalid_impl(set, i)) return (int)i;
-	}
-	return 0;
-}
-/*==========================================================
- * xref_prev_impl -- Return prev valid of some type before i
- *  returns 0 if none found
- *  generic for all 5 types
- *========================================================*/
-static int32_t
-xref_prev_impl (DELETESET set, int32_t i)
-{
-	if (set->n == set->recs[0]) return 0; /* no valids */
-	while (--i)
-	{
-		if (xref_isvalid_impl(set, i)) return i;
-	}
-	return 0;
-}
-/*===============================================
- * xref_next? -- Return next valid indi/? after i
- *  returns 0 if none found
- *  5 symmetric versions
- *=============================================*/
-int xref_nexti (int i) { return (int)xref_next_impl(&irecs, (int32_t)i); }
-int xref_nextf (int i) { return (int)xref_next_impl(&frecs, (int32_t)i); }
-int xref_nexts (int i) { return (int)xref_next_impl(&srecs, (int32_t)i); }
-int xref_nexte (int i) { return (int)xref_next_impl(&erecs, (int32_t)i); }
-int xref_nextx (int i) { return (int)xref_next_impl(&xrecs, (int32_t)i); }
-int xref_next (char ntype, int i)
-{
-	switch(ntype) {
-	case 'I': return xref_nexti(i);
-	case 'F': return xref_nextf(i);
-	case 'S': return xref_nexts(i);
-	case 'E': return xref_nexte(i);
-	case 'X': return xref_nextx(i);
-	}
-	ASSERT(0); return 0;
-}
-/*================================================
- * xref_prev? -- Return prev valid indi/? before i
- *  returns 0 if none found
- *  5 symmetric versions
- *==============================================*/
-int xref_previ (int i) { return (int)xref_prev_impl(&irecs, (int32_t)i); }
-int xref_prevf (int i) { return (int)xref_prev_impl(&frecs, (int32_t)i); }
-int xref_prevs (int i) { return (int)xref_prev_impl(&srecs, (int32_t)i); }
-int xref_preve (int i) { return (int)xref_prev_impl(&erecs, (int32_t)i); }
-int xref_prevx (int i) { return (int)xref_prev_impl(&xrecs, (int32_t)i); }
-int xref_prev (char ntype, int i)
-{
-	switch(ntype) {
-	case 'I': return xref_previ(i);
-	case 'F': return xref_prevf(i);
-	case 'S': return xref_prevs(i);
-	case 'E': return xref_preve(i);
-	case 'X': return xref_prevx(i);
-	}
-	ASSERT(0); return 0;
-}
-/*=========================================
- * xref_first? -- Return first valid indi/?
- *  returns 0 if none found
- *  5 symmetric versions
- *=======================================*/
-int xref_firsti (void) { return xref_nexti(0); }
-int xref_firstf (void) { return xref_nextf(0); }
-int xref_firsts (void) { return xref_nexts(0); }
-int xref_firste (void) { return xref_nexte(0); }
-int xref_firstx (void) { return xref_nextx(0); }
-/*=======================================
- * xref_last? -- Return last valid indi/?
- *  returns 0 if none found
- *  5 symmetric versions
- *=====================================*/
-static int xref_last (DELETESET set)
-{
-	return (int)xref_prev_impl(set, set->recs[0]);
-}
-int xref_lasti (void) { return xref_last(&irecs); }
-int xref_lastf (void) { return xref_last(&frecs); }
-int xref_lasts (void) { return xref_last(&srecs); }
-int xref_laste (void) { return xref_last(&erecs); }
-int xref_lastx (void) { return xref_last(&xrecs); }
-
-#if !defined(DEADENDS)
-/*=======================================
- * xrefs_get_counts_from_unopened_db --
- *  read record counts out of file on disk
- * returns false if specified path is not the root of a traditional lifelines database
- * If db structure error, errptr points to description of error (in static buffer)
- *=====================================*/
-bool
-xrefs_get_counts_from_unopened_db (CString path, int *nindis, int *nfams
-	, int *nsours, int *nevens, int *nothrs, char ** errptr)
-{
-	char scratch[MAXPATHLEN];
-	static char errstr[256];
-	FILE * fp = 0;
-	int i;
-	int32_t ndels[5], nmax[5];
-
-	*errptr = 0;
-
-	ASSERT(!xreffp);
-	snprintf(scratch, sizeof(scratch), "%s/xrefs", path);
-	if (!(fp = fopen(scratch, LLREADBINARY))) {
-		return false;
-	}
-	for (i=0; i<5; ++i) {
-		if (fread(&ndels[i], sizeof(int32_t), 1, fp) != 1) {
-			snprintf(errstr, sizeof(errstr), "ndels[" FMT_INT "] bad", i);
-			*errptr = errstr;
-			fclose(fp);
-			return false;
-		}
-	}
-	for (i=0; i<5; ++i) {
-		int32_t j;
-		for (j=0; j<ndels[i]; ++j) {
-			int32_t k;
-			if (fread(&k, sizeof(int32_t), 1, fp) != 1) {
-				snprintf(errstr, sizeof(errstr), "ndels[" FMT_INT "]#" FMT_INT32 " bad", i, j);
-				*errptr = errstr;
-				fclose(fp);
-				return false;
-			}
-			if (!j)
-				nmax[i] = k;
-		}
-	}
-	/* This logic is similar to what is in xref_num_set() */
-	*nindis = (int)(nmax[0] - ndels[0]);
-	*nfams  = (int)(nmax[1] - ndels[1]);
-	*nevens = (int)(nmax[2] - ndels[2]);
-	*nsours = (int)(nmax[3] - ndels[3]);
-	*nothrs = (int)(nmax[4] - ndels[4]);
-	fclose(fp);
-	return true;
-}
-#endif
