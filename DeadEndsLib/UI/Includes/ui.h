04c4074228d0277311e495e6e7c6afa9a00e6a8c
David Taylor <dctaylor87@gmail.com>
2025-04-10 12:31:16 -0400

    Add forward reference.

--- DeadEndsLib/UI/Includes/ui.h
+++ DeadEndsLib/UI/Includes/ui.h
@@ -36,6 +36,7 @@ bool ask_for_filename_impl(CString ttl, CString path, CString prmpt, String buff
 bool ask_for_program (CString mode, CString ttl, String *pfname, String *pfullpath, CString path, CString ext, bool picklist);
 int chooseFromArray (CString ttl, int no, String *pstrngs);
 int chooseFromList (CString ttl, List *list);
+enum SequenceType;		/* forward reference */
 int chooseListFromSequence (CString ttl, Sequence *seq, enum SequenceType type);
 int chooseOneFromSequence (CString ttl, Sequence *seq, enum SequenceType type);
 int chooseOneOrListFromSequence(CString ttl, Sequence *seq, bool multi, enum SequenceType type); /* XXX */
