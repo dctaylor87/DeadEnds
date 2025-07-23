e70a1dd465fd1e90e773f22c825425fb0e118763
David Taylor <dctaylor87@gmail.com>
2025-07-23 17:08:06 -0400

    New functions: choosechild, chooseindi, choosespouse, choosesubset, choosefam, genindiset

--- DeadEndsLib/Interp/extra.c
+++ DeadEndsLib/Interp/extra.c
@@ -24,6 +24,9 @@
 #include "date.h"
 #include "place.h"
 #include "builtintable.h"
+#include "sequence.h"
+#include "gedcom.h"
+
 #include "ask.h"
 #include "rptui.h"
 #include "denls.h"
@@ -32,10 +35,14 @@
 #include "messages.h"
 #include "de-strings.h"
 #include "feedback.h"
+#include "generic.h"
+#include "ll-sequence.h"
 
+#if 0				/* unused, Tom has now implemented __index */
 static void compute_pi(String pi, CString sub);
 static int ll_index(CString str, CString sub, int num);
 static int kmp_search(CString pi, CString str, CString sub, int num);
+#endif
 
 // * __getindi -- Have user identify person
 // *   usage: getindi(IDEN [,STRING]) --> VOID
@@ -77,54 +84,195 @@ PValue __getindi (PNode *pnode, Context *context, bool* eflg)
 /* __choosechild -- have user choose child of person or family.
    usage: choosechild (INDI|FAM) --> INDI.  */
 
-PValue __choosechild (PNode *pnode, Context *context, bool* eflg)
+PValue __choosechild (PNode *pnode, Context *context, bool* errflg)
 {
+  PValue gnv;
+  RecordType type;
+  Database *database = context->database; /* XXX allow database as an optional 2nd argument XXX */
+
+  gnv = evaluate (pnode->arguments, context, errflg);
+  if (*errflg)
+    {
+      scriptError (pnode, "Error evaluating argument to choosechild.");
+      return nullPValue;
+    }
+  if (gnv.type == PVPerson)
+    type = GRPerson;
+  else if (gnv.type == PVFamily)
+    type = GRFamily;
+  else if (gnv.type == PVGNode)
+    {
+      switch (recordType (gnv.value.uGNode))
+	{
+	case GRPerson:
+	  type = GRPerson;
+	  break;
+	case GRFamily:
+	  type = GRFamily;
+	  break;
+	default:
+	  scriptError (pnode, "Argument to choosechild must be either a family or a person record.");
+	  *errflg = true;
+	  return nullPValue;
+	}
+    }
+  else
+    {
+      scriptError (pnode, "Argument to choosechild must be either a family or a person record.");
+      *errflg = true;
+      return nullPValue;
+    }
+
+  Sequence *seq;
+  if (type == GRPerson)
+    seq = personToChildren (gnv.value.uGNode, database->recordIndex);
+  else
+    seq = familyToChildren (gnv.value.uGNode, database->recordIndex);
+
+  if (! seq || (lengthSequence (seq) < 1))
+    return nullPValue;		/* NOT an error -- person/family might have no children */
+  GNode *indi = chooseFromSequence (seq, DOASK1, _(qSifonei), _(qSnotonei), chooseTypeDefault);
+  if (! indi)
+    return nullPValue;		/* user cancelled */
+
+  return PVALUE(PVPerson, uGNode, indi);
 }
 
 
 /* __chooseindi -- have user choose person from a set
    usage: chooseindi (SET) --> INDI.  */
 
-PValue __chooseindi (PNode *pnode, Context *context, bool* eflg)
+PValue __chooseindi (PNode *pnode, Context *context, bool* errflg)
 {
-#if 0
-  PNODE argvar = builtin_args(node);
-  NODE indi=0;
-  INDISEQ seq=0;
-  PVALUE val = eval_and_coerce(PSET, argvar, stab, eflg);
-  if (*eflg) {
-    prog_var_error(node, stab, argvar, val, nonset1, "chooseindi");
-    delete_pvalue_ptr(&val);
-    return NULL;
-  }
-  seq = pvalue_to_seq(val);
-  delete_pvalue_ptr(&val);
-  if (!seq || length_indiseq(seq) < 1) return NULL;
-  indi = nztop(choose_from_indiseq(seq, DOASK1, _(qSifonei), _(qSnotonei)));
-  if (!indi) return NULL;
-  return create_pvalue_from_indi(indi);
-#endif
+  PValue svalue = evaluate (pnode->arguments, context, errflg);
+  if (*errflg || (svalue.type != PVSequence))
+    {
+      scriptError (pnode, "Argument to chooseindi must be a set.");
+      *errflg = true;
+      return nullPValue;
+    }
+  Sequence *seq = svalue.value.uSequence;
+  if (! seq || (lengthSequence (seq) < 1))
+    {
+      scriptError (pnode, "Argument to chooseindi must be a non-empty set.");
+      *errflg = true;
+      return nullPValue;
+    }
+  GNode *indi = chooseFromSequence (seq, DOASK1, _(qSifonei), _(qSnotonei), chooseTypeDefault);
+  if (! indi)
+    return nullPValue;
+
+  return PVALUE(PVPerson, uGNode, indi);
 }
 
 /* __choosespouse -- have user choose spouse of person.
    usage: choosespouse(INDI|FAM) --> INDI.  */
 
-PValue __choosespouse (PNode *pnode, Context *context, bool* eflg)
+PValue __choosespouse (PNode *pnode, Context *context, bool* errflg)
 {
+  GNode *fam = 0;
+  GNode *indi = 0;
+  Database *database = context->database; /* XXX make database an optional argument XXX */
+
+  PValue pvalue = evaluate (pnode->arguments, context, errflg);
+  if (*errflg)
+    {
+      scriptError (pnode, "Error evaluating argument to choosespouse.");
+      return nullPValue;
+    }
+  switch (pvalue.type)
+    {
+    case GRPerson:
+      indi = pvalue.value.uGNode;
+      break;
+    case GRFamily:
+      fam = pvalue.value.uGNode;
+      break;
+    default:
+      scriptError (pnode, "Argument to choosespouse must be either a person or a family.");
+      *errflg = true;
+      return nullPValue;
+    }
+  if (! indi && ! fam)
+    {
+      /* null GNode* */
+      scriptError (pnode, "Argument to choosespouse must be a non-null person or family.");
+      *errflg = true;
+      return nullPValue;
+    }
+  Sequence *seq;
+
+  if (indi)
+    seq = personToSpouses (indi, database->recordIndex);
+  else
+    seq = familyToSpouses (fam, database);
+
+  if (lengthSequence (seq) < 1)
+    return nullPValue;		/* no spouses */
+
+  GNode *newindi = chooseFromSequence (seq, DOASK1, _(qSifonei), _(qSnotonei), chooseTypeDefault);
+  if (newindi)
+    return PVALUE (PVPerson, uGNode, newindi);
+  else
+    return nullPValue;
 }
 
 /* __choosesubset -- have user choose subset from a set.
    usage: choosesubset (SET) --> SET.  */
 
-PValue __choosesubset (PNode *pnode, Context *context, bool* eflg)
+PValue __choosesubset (PNode *pnode, Context *context, bool* errflg)
 {
+  PValue svalue = evaluate (pnode->arguments, context, errflg);
+  if (*errflg || (svalue.type != PVSequence))
+    {
+      scriptError (pnode, "Argument to choosesubset must be a set.");
+      *errflg = true;
+      return nullPValue;
+    }
+  Sequence *seq = svalue.value.uSequence;
+  if (! seq || (lengthSequence (seq) < 1))
+    {
+      scriptError (pnode, "Argument to choosesubset must be a non-empty set.");
+      *errflg = true;
+      return nullPValue;
+    }
+  Sequence *newseq = copySequence (seq);
+  int retval = chooseListFromSequence (_(qSnotonei), newseq, chooseTypeDefault);
+  if (retval == -1)
+    return nullPValue;
+
+  return PVALUE(PVSequence, uSequence, newseq);
 }
 
 /* __choosefam -- have user choose family of person.
    usage: choosefam (INDI) --> FAM.  */
 
-PValue __choosefam (PNode *pnode, Context *context, bool* eflg)
+PValue __choosefam (PNode *pnode, Context *context, bool* errflg)
 {
+  PValue indi = evaluate (pnode->arguments, context, errflg);
+  Database *database = context->database;
+
+  if (*errflg)
+    {
+      scriptError (pnode, "Error evaluating argument to choosefam.");
+      return nullPValue;
+    }
+  if (indi.type != PVPerson)
+    {
+      scriptError (pnode, "Argument to choosefam must be a person.");
+      *errflg = true;
+      return nullPValue;
+    }
+
+  Sequence *seq = personToFamilies (indi.value.uGNode, false, database->recordIndex);
+
+  if (! seq)
+    return nullPValue;		/* not an error, indi is not a spouse in any family */
+  GNode *fam = chooseFromSequence (seq, DOASK1, _(qSifonei), _(qSnotonei), chooseTypeDefault);
+  if (fam)
+    return PVALUE (PVFamily, uGNode, fam);
+  else
+    return nullPValue;
 }
 
 /* __getindiset -- have user identify set of persons
@@ -156,6 +304,24 @@ PValue __getindiset (PNode *pnode, Context *context, bool* errflg)
   assignValueToSymbol (context, iden->identifier,
 		       PVALUE (PVSequence, uSequence, seq));
   return nullPValue;
+}
+
+/* __genindiset -- identify set of persons that 'match' a string
+   usage: genindiset (STRING) --> SET.  */
+PValue __genindiset (PNode *pnode, Context *context, bool *errflg)
+{
+  CString string = evaluateString (pnode->arguments, context, errflg);
+  if (*errflg || (! string) || (*string == 0))
+    {
+      scriptError (pnode, "The argument to genindiset must be a non-empty string.");
+      *errflg = true;
+      return nullPValue;
+    }
+  Sequence *seq = stringToSequence (string, context->database);
+  if (! seq || (lengthSequence (seq) <= 0))
+    return nullPValue;		/* not an error -- no matches */
+
+  return PVALUE (PVSequence, uSequence, seq);
 }
 
 /* getint (variable [,string]) --> void
@@ -230,6 +396,7 @@ PValue __getstr (PNode *pnode, Context *context, bool* eflg)
   return nullPValue;
 }
 
+#if 0				/* renamed -- Tom has now implemented __index */
 /* index(str, sub, occurrence) --> int
 
    returns the 1-based index of the 'occurrence'th instance of sub
@@ -323,6 +490,7 @@ compute_pi (String pi, CString sub)
 		pi[q] = k;
 	}
 }
+#endif
 
 /* __getproperty -- return property string.
 
