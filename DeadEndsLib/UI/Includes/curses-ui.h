e4a9a33c5936e2f2663e146d9504058c52242d35
David Taylor <dctaylor87@gmail.com>
2025-08-06 12:40:22 -0400

    Fix decl of curses_invoke_search_menu

--- DeadEndsLib/UI/Includes/curses-ui.h
+++ DeadEndsLib/UI/Includes/curses-ui.h
@@ -9,7 +9,7 @@ extern bool curses_ask_for_string2 (CString ttl1, CString ttl2, CString prmpt,
 				    String buffer, int buflen);
 extern int curses_chooseFromArray (CString ttl, int no, String *pstrngs);
 extern int curses_prompt_stdout (CString prompt);
-extern Sequence curses_invoke_search_menu (void);
+extern Sequence *curses_invoke_search_menu (void);
 extern void curses_llvwprintf (CString fmt, va_list args);
 extern void curses_view_array (CString ttl, int no, String *pstrngs);
 extern bool curses_ask_for_program (CString mode, CString ttl, String *pfname,
