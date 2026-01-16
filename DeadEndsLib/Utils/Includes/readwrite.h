4c4e89b57ec2db129adfad99b5644553357c5948
David Taylor <dctaylor87@gmail.com>
2026-01-16 11:17:49 -0500

    Move valid decls from readwrite.h to (for now) validate.h.

--- DeadEndsLib/Utils/Includes/readwrite.h
+++ DeadEndsLib/Utils/Includes/readwrite.h
@@ -22,22 +22,3 @@
 
 #define FMT_INT		"%d"
 #define SCN_INT		"%d"
-
-extern String editfile;
-extern String editstr;
-
-/* valid.c */
-//extern bool pointer_value(String);
-extern bool valid_indi_tree(GNode *, String*, GNode *, Database *database);
-extern bool valid_fam_tree(GNode *, String*, GNode *, Database *database);
-extern bool valid_name(String);
-extern bool valid_node_type(GNode *node, char ntype, String *pmsg,
-			    GNode *node0, Database *database);
-extern bool valid_sour_tree(GNode *, String*, GNode *, Database *database);
-extern bool valid_even_tree(GNode *, String*, GNode *, Database *database);
-extern bool valid_othr_tree(GNode *, String*, GNode *, Database *database);
-
-/* write.c */
-
-/* XXX interface and/or name might change XXX */
-extern GNode *file_to_node (String fname, XLAT ttmi, String *pmsg, bool *pemp);
--- DeadEndsLib/Validate/Includes/validate.h
+++ DeadEndsLib/Validate/Includes/validate.h
@@ -24,10 +24,35 @@ typedef enum ValidationCodes {
 
 extern void validatePersons(RecordIndex*, CString name, IntegerTable*, ErrorLog*);
 extern void validateFamilies(RecordIndex*, CString name, IntegerTable*, ErrorLog*);
+extern bool validateRecord (GNode *, Database *, ErrorLog *);
 extern RefnIndex* getReferenceIndex(RecordIndex*, CString name, IntegerTable*, ErrorLog*);
 extern bool validateSourceIndex(Database* database, ErrorLog* errorLog);
 extern bool validateEventIndex(Database* database, ErrorLog* errorLog);
 extern bool validateOtherIndex(Database* database, ErrorLog* errorLog);
 extern int rootLine(GNode*, IntegerTable*);
 
+/* valid.c */
+//extern bool pointer_value(String);
+extern bool validateNewPerson(GNode *new, GNode *old,
+			      Database *database, ErrorLog *errorLog);
+extern bool validateNewFamily(GNode *new, GNode *old,
+			      Database *database, ErrorLog *errorLog);
+#if 0
+extern bool valid_name(String);
+#endif
+extern bool validateNewRecord(GNode *newNode, GNode *origNode, RecordType ntype,
+			      Database *database, ErrorLog *errorLog);
+extern bool validateNewSource(GNode *new, GNode *orig,
+			      Database *database, ErrorLog *errorLog);
+extern bool validateNewEvent(GNode *new, GNode *orig,
+			     Database *database, ErrorLog *errorLog);
+extern bool validateNewOther(GNode *new, GNode *orig,
+			     Database *database, ErrorLog *errorLog);
+
+//#if 0
+/* write.c */
+
+/* XXX interface and/or name might change XXX */
+extern GNode *file_to_node (String fname, String *pmsg, bool *pemp);
+//#endif
 #endif // validate_h
