bf0bfb9d87b95d6581ce7abd550f493e03a40b09
David Taylor <dctaylor87@gmail.com>
2026-02-12 16:29:56 -0500

    Improve comments.

--- Lifelines/Includes/ll-standard.h
+++ Lifelines/Includes/ll-standard.h
@@ -1,14 +1,14 @@
-/* looking at the Lifelines code, a keynum is sometimes an int (32
-   bits), sometimes an int64_t (64 bits, obviously), and sometimes an
-   INT (which is 32 bits [x86] or 64 bits [x86_64] -- depending on
-   platform) */
+// In the Lifelines code, a keynum is sometimes an int (32
+// bits), sometimes an int64_t (64 bits, obviously), and sometimes an
+// INT (which is 32 bits [x86] or 64 bits [x86_64] -- depending on
+// platform).  In the DeadEnds port, a keynum is *ALWAYS* a uint64_t.
 
 typedef int64_t		KEYNUM_TYPE;
-
 #define MAXKEYNUMBER	999999999999L
-/* XXX not sure if MAXKEYWIDTH is still used.  Have eliminated most uses.  XXX */
-/* was 13, changed to 26 in case we switch from uint32_t to uint64_t */
-#define MAXKEYWIDTH	26	/* does NOT include NUL nor surrounding @'s */
+
+// Have eliminated most uses or MAXKEYWIDTH.
+// Was 13, changed to 26 due to the switch to uint64_t; 26 includes some padding.
+#define MAXKEYWIDTH	26	// does NOT include NUL nor surrounding @'s
 
 #define PRIdKEYNUM	PRId64
 #define FMT_KEYNUM	"%" PRIdKEYNUM
